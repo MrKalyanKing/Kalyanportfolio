0296a80a204d381ae3d098f262dbac062257c770
MrKalyanNick <badhavathkalyan08@gmail.com>
2025-01-10 23:43:28 +0530

    frist commit

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -9,7 +9,7 @@ import { fadeIn, textVariant } from "../utils/motion";
 import pdf from '../assets/KalyanNewResume-1.pdf'
 // ServiceCard Component
 const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className="xs:w-[250px] w-full">
+  <div className="xs:w-[250px] w-full">
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
       className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
@@ -26,7 +26,7 @@ const ServiceCard = ({ index, title, icon }) => (
         <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
       </div>
     </motion.div>
-  </Tilt>
+  </div>
 );
 
 // About Component
